7db59be56936f283908e307ca454551b204d7ca8
Derek <dqi@toronto.ca>
2026-02-12 14:14:09 -0500

    replaced argparse with docopt

--- konsync/__main__.py
+++ konsync/__main__.py
@@ -1,97 +1,44 @@
 '''Konsync entry point.'''
 
-import argparse, os, shutil
+import docopt, os, shutil
 from importlib.resources import files
 from pathlib import Path
+
+from epicstuff import Dict, s
 
 from konsync.consts import CONFIG_FILE, VERSION
 from konsync.funcs import export, log, sync
 
 
-def _get_parser() -> argparse.ArgumentParser:
-	'''Return CLI parser.
+def _get_parser(argv: list[str] | str | None = None) -> docopt.ParsedOptions:
+	'Return CLI parser.'
+	doc = s('''
+		konsync
+		A simple and powerful utility for managing your dotfiles.
 
-	Return:
-		argparse.ArgumentParser: Created parser.
+		Usage:
+			konsync <command> [options...]
+			konsync -h, --help
+			konsync --version
 
-	'''
-	parser = argparse.ArgumentParser(
-		prog='Konsync',
-		description='A simple and powerful utility for managing your dotfiles.',
-		epilog='Please report bugs at https://www.github.com/epicstuff/konsync/issues',
-		usage='%(prog)s [options...] [location]',
-	)
+		Commands:
+			sync, s     Setup sync based on current config
+			export, e   Export and compress files specified in config
+			import, i   Import files that were exported
+			remove, r   Remove links and copies files
 
-	parser.add_argument(
-		'-s',
-		'--sync',
-		required=False,
-		action='store_true',
-		help='Setup sync based on current config',
-	)
-	parser.add_argument(
-		'-r',
-		'--remove',
-		required=False,
-		action='store_true',
-		help='Remove links and copies files',
-	)
-	parser.add_argument(
-		'-e',
-		'--export',
-		required=False,
-		action='store_true',
-		help='Export and compress files that are not synced',
-	)
-	parser.add_argument(
-		'-i',
-		'--import',
-		required=False,
-		action='store_true',
-		help='Import files that are not synced',
-	)
-	parser.add_argument(
-		'-f',
-		'--force',
-		required=False,
-		help='Force, will delete existing files, specify to wether prioritise local or sync files',
-		choices=['local', 'sync', ''],
-		nargs='?',
-		const=True,
-	)
-	parser.add_argument(
-		'-v',
-		'--version',
-		'--verbose',
-		required=False,
-		action='store_true',
-		help='Show version when used without other aguments, acts as verbose switch when used with other aguments',
-	)
-	parser.add_argument(
-		'-c',
-		'--config',
-		required=False,
-		type=Path,
-		help='Specify config file location, defaults to ./config.taml',
-	)
-	parser.add_argument(
-		'-C',
-		'--compression',
-		required=False,
-		type=str,
-		default='fpaq',
-		help='Specify compression algorithm, overwriting config.taml',
-		choices=['fpaq'],
-	)
-	parser.add_argument(
-		'location',
-		nargs='?',
-		type=Path,
-		help='Specify directory to sync files to, overwrites config.taml',
-	)
+		Options:
+			-h, --help                  Show this.
+			--version                   Show version.
+			-v, --verbose               Enable verbose output.
+			-f, --force <mode>          Force, will delete existing files, specify to wether prioritizes local or sync files. [local | sync]
+			-c, --config <file>         Specify config file location, defaults to ./config.taml
+			-C, --compression <method>  Specify compression algorithm, overwriting config.taml [fpaq]
+			-l, --location <dir>        Specify directory to sync files to, overwrites config.taml
 
-	return parser
-
+		Please report bugs at https://www.github.com/epicstuff/konsync/issues
+	''').replace('\t', '    ')
+	return docopt.docopt(doc, argv, version=VERSION)
 
 def main() -> None:
 	'''Handle the arguments and options.'''
@@ -105,24 +52,27 @@ def main() -> None:
 			shutil.copy(default_config_path, CONFIG_FILE)
 		log.info('created config file')
 
-	parser = _get_parser()
-	args = parser.parse_args()
+	# parse command line arguments
+	args = _get_parser()
+	args = Dict({'compression': args['--compression'], 'config': args['--config'], 'force': args['--force'], 'location': args['--location'], 'verbose': args['--verbose'], 'command': args['<command>']})
 
 	# set log level based on verbose (version)
-	if args.version:
+	if args.verbose:
 		log.setLevel('DEBUG')
 	else:
 		log.setLevel('INFO')
-	if args.sync:
+	if args.command in {'sync', 's'}:
 		sync(args.config, args.location, args.version, args.force)
-	elif args.export:
+	elif args.command in {'export', 'e'}:
 		export(args.config, args.location, args.compression, args.version)
-	# elif args.remove:
-	# 	remove()
+	elif args.command in {'import', 'i'}:
+		...
+	elif args.command in {'remove', 'r'}:
+		...
 	elif args.version:
 		print(VERSION)
 	else:
-		parser.print_help()
+		raise Exception('TODO: look into this')
 
 
 if __name__ == '__main__':
